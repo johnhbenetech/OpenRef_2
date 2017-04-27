10dc216a4fb6d569c7ff01a9d90902d0a7adb4e3
John Higgins <johnh@benetech.org>
2017-04-26 20:07:46 -0700

    .

--- config/openref/admin.py
+++ config/openref/admin.py
@@ -3,17 +3,17 @@ from reversion.admin import VersionAdmin
 from .models import Provider, ProviderUpdate
 
 
-
 admin.site.site_header = 'My administration'
 admin.site.site_title= 'Admin'
 admin.site.index_title= 'Admin Actions'
 
 
-class ProviderUpdateAdmin(VersionAdmin, admin.ModelAdmin):
-    list_display = ('name','price')
-    list_filter = ('price',)
-    search_fields = ('name','price')
-    readonly_fields = ('created_by',)
+
+
+class ProviderUpdateAdmin(VersionAdmin, admin.ModelAdmin): 
+    list_display = ('name','description','price')
+    exclude = ('created_by',)    
+    
     def get_queryset(self,request):
         qs = super(ProviderUpdateAdmin, self).get_queryset(request)
         if request.user.is_superuser:
@@ -22,14 +22,15 @@ class ProviderUpdateAdmin(VersionAdmin, admin.ModelAdmin):
         # we're done. Assumption: Page.owner is a foreignkey
         # to a User.
         return qs.filter(provider__owner=request.user)
-        
+            
     def formfield_for_foreignkey(self,db_field, request, **kwargs):
         if db_field.name == 'provider':
             if not request.user.is_superuser:
                 kwargs["queryset"] = Provider.objects.filter(
                     owner=request.user)
+                kwargs['initial'] = 1
         return super(ProviderUpdateAdmin, self).formfield_for_foreignkey(
-            db_field, request, **kwargs)
+            db_field, request, **kwargs)   
             
     def save_model(self, request, obj, form, change):
         if not change:
@@ -41,10 +42,10 @@ class ProviderUpdateAdmin(VersionAdmin, admin.ModelAdmin):
 admin.site.register(ProviderUpdate,ProviderUpdateAdmin)
 
 class ProviderAdmin(VersionAdmin, admin.ModelAdmin):
-    list_display = ('name','price')
+    list_display = ('name','price','owner')
     list_filter = ('price',)
     search_fields = ('name','price')
-    readonly_fields = ('created_by',)
+    exclude = ('created_by',)
 
     def get_queryset(self,request):
         qs = super(ProviderAdmin, self).get_queryset(request)
@@ -62,4 +63,4 @@ class ProviderAdmin(VersionAdmin, admin.ModelAdmin):
         obj.save()
 	
 	
-admin.site.register(Provider, ProviderAdmin)
+admin.site.register(Provider,ProviderAdmin)
--- config/openref/models.py
+++ config/openref/models.py
@@ -28,11 +28,11 @@ class ProviderUpdate(models.Model):
         choices=STATUS_CHOICES,
         default=STATUS_NONE,
     )
-    provider = models.ForeignKey(Provider, related_name='updates')
+    provider = models.ForeignKey(Provider, related_name='updates',default=1)
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=20)
     created_by = models.ForeignKey(User)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
-	
+    
